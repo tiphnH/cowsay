97ba845faf29f1eac7afdda80a40e5651f4e34de
Tiphaine HOGREL <hogrelt@gmail.com>
2022-05-24 14:37:30 +0200

    premier commit

---
+++ index.js
@@ -0,0 +1,10 @@
+const student = require("./information");
+let cowsay = require("cowsay");
+
+console.log(
+  cowsay.say({
+    text: `Je suis ${student.name}, du campus ${student.campus}`,
+    e: "oO",
+    T: "U ",
+  })
+);
